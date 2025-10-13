f2e78294253a9aca3ee8b578c4478fccee8c5a22
NoX <nox@noxsoftware.pl>
2025-10-13 13:46:39 +0200

    stuff + routing

--- index.php
+++ index.php
@@ -1,42 +1,9 @@
 <?php
 
-$path = trim($_SERVER['REQUEST_URI']);
+require_once "Routing.php";
 
-$path = parse_url($path);
-var_dump($path);
+$path = parse_url($_SERVER['REQUEST_URI']);
+$path = trim($path['path'], '/');
 
-switch ($path)
-{
-        case 'dashboard':
-                echo "
-                <style>
-                        h2 {
-                                color: red;
-                        }
-                </style>
-                ";
-                echo "<h1>DASHBOARD</h1>";
-        case 'login':
-                echo "
-                <style>
-                        h2 {
-                                color: blue;
-                        }
-                </style>
-                ";
-                echo "<h1>LOGIN</h1>";
-        default:
-                echo "<h1>404</h1>";
-}
-
-
-echo "
-<script>
-        const header = document.querySelector('h1');
-        console.log(header);
-        header.addEventListener('click', () => {
-                header.style.color = 'green';
-        });
-</script>
-"
+Routing::run($path);
 ?>
